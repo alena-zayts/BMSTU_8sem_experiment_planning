b3e1a06dd6a2354e0dfffbddb120d99fc466f713
Alena Zaytseva <alena_zaytseva_02@mail.ru>
2023-03-25 22:20:14 +0300

    start 3

--- lab3/horse.py
+++ lab3/horse.py
@@ -1,49 +1,118 @@
-from lab2.queue.processor import Processor
-from lab2.queue.generator import Generator
-from lab2.queue.modeller import Modeller
-from lab2.queue import distributions
+from lab3.queue.processor import Processor
+from lab3.queue.generator import Generator
+from lab3.queue.modeller import Modeller
+from lab3.queue import distributions
 import numpy as np
 
-FACTORS_NUMBER = 3
+FACTORS_NUMBER = 6
 M_SIZE = 2 ** FACTORS_NUMBER
 
 LIN_COEFS_AMOUNT = FACTORS_NUMBER + 1
 NONLIN_COEFS_AMOUNT = M_SIZE
 
-N_REPEATS = 3
+N_REPEATS = 1
 
 
 class Horse:
+    # x1 - gen_int
+    # x2 - proc_int
+    # x3 - proc_var
+    # x4 - gen_int2
+    # x5 - proc_int2
+    # x6 - proc_var2
+
     def __init__(self, gen_int_min, gen_int_max, proc_int_min, proc_int_max, proc_var_min, proc_var_max,
+                 gen_int_min2, gen_int_max2, proc_int_min2, proc_int_max2, proc_var_min2, proc_var_max2):
+        self.global_gen_int_min = gen_int_min
+        self.global_gen_int_max = gen_int_max
+        self.global_proc_int_min = proc_int_min
+        self.global_proc_int_max = proc_int_max
+        self.global_proc_var_min = proc_var_min
+        self.global_proc_var_max = proc_var_max
+
+        self.global_gen_int_min2 = gen_int_min2
+        self.global_gen_int_max2 = gen_int_max2
+        self.global_proc_int_min2 = proc_int_min2
+        self.global_proc_int_max2 = proc_int_max2
+        self.global_proc_var_min2 = proc_var_min2
+        self.global_proc_var_max2 = proc_var_max2
+
+        # self.coefficients = []
+
+
+    def load_cur_min_maxes(self, gen_int_min, gen_int_max, proc_int_min, proc_int_max, proc_var_min, proc_var_max,
+                 gen_int_min2, gen_int_max2, proc_int_min2, proc_int_max2, proc_var_min2, proc_var_max2,
                  requests_amount):
         self.gen_int_min = gen_int_min
         self.gen_int_max = gen_int_max
-
         self.proc_int_min = proc_int_min
         self.proc_int_max = proc_int_max
         self.proc_var_min = proc_var_min
         self.proc_var_max = proc_var_max
 
+        self.gen_int_min2 = gen_int_min2
+        self.gen_int_max2 = gen_int_max2
+        self.proc_int_min2 = proc_int_min2
+        self.proc_int_max2 = proc_int_max2
+        self.proc_var_min2 = proc_var_min2
+        self.proc_var_max2 = proc_var_max2
+
         self.requests_amount = requests_amount
-        self.coefficients = []
 
-    def create_plan_matrix(self):
-        matrix = np.array([[1] * M_SIZE for _ in range(M_SIZE)])
+    def create_plan_matrix(self, min_maxes):  # min_maxes: [[[min1, max1], [min2, max2], ...]
+        # fill everything with minimums
+        matrix = np.array([
+            [1] +  # x0
+            [min_maxes[factor_index][0] for factor_index in range(FACTORS_NUMBER)] +
+            [1] * (M_SIZE - FACTORS_NUMBER - 1)  # temporary (just to give space)
 
-        for j in range(1, FACTORS_NUMBER + 1):
-            period = pow(2, FACTORS_NUMBER - j)
+        ])
+        # old matrix = np.array([[1] * M_SIZE for row_number in range(M_SIZE)])
+
+        for factor_index in range(1, FACTORS_NUMBER + 1):
+            period = pow(2, FACTORS_NUMBER - factor_index)
             for start_minus in range(period, M_SIZE, 2 * period):
-                for i in range(start_minus, start_minus + period):
-                    matrix[i][j] = -1
-        matrix *= -1
-        matrix[:, 0] *= -1
-        # pairs
-        matrix[:, 4] = matrix[:, 1] * matrix[:, 2]
-        matrix[:, 5] = matrix[:, 1] * matrix[:, 3]
-        matrix[:, 6] = matrix[:, 2] * matrix[:, 3]
+                for row_number in range(start_minus, start_minus + period):
+                    matrix[row_number][factor_index] = min_maxes[factor_index][1]  # add maxes
 
-        # triplets
-        matrix[:, 7] = matrix[:, 1] * matrix[:, 2] * matrix[:, 3]
+        # 2
+        cur_factors_mult_index = 7
+        for factor_index1 in range(1, FACTORS_NUMBER):
+            for factor_index2 in range(factor_index1 + 1, FACTORS_NUMBER + 1):
+                matrix[:, cur_factors_mult_index] = matrix[:, factor_index1] * matrix[:, factor_index2]
+                cur_factors_mult_index += 1
+
+        # 3
+        for factor_index1 in range(1, FACTORS_NUMBER - 1):
+            for factor_index2 in range(factor_index1 + 1, FACTORS_NUMBER):
+                for factor_index3 in range(factor_index2 + 1, FACTORS_NUMBER + 1):
+                    matrix[:, cur_factors_mult_index] = (matrix[:, factor_index1] * matrix[:, factor_index2] *
+                                                         matrix[:, factor_index3])
+                    cur_factors_mult_index += 1
+
+        # 4
+        for factor_index1 in range(1, FACTORS_NUMBER - 2):
+            for factor_index2 in range(factor_index1 + 1, FACTORS_NUMBER - 1):
+                for factor_index3 in range(factor_index2 + 1, FACTORS_NUMBER):
+                    for factor_index4 in range(factor_index3 + 1, FACTORS_NUMBER + 1):
+                        matrix[:, cur_factors_mult_index] = (matrix[:, factor_index1] * matrix[:, factor_index2] *
+                                                             matrix[:, factor_index3] * matrix[:, factor_index4])
+                        cur_factors_mult_index += 1
+
+        # 5
+        for factor_index1 in range(1, FACTORS_NUMBER - 3):
+            for factor_index2 in range(factor_index1 + 1, FACTORS_NUMBER - 2):
+                for factor_index3 in range(factor_index2 + 1, FACTORS_NUMBER - 1):
+                    for factor_index4 in range(factor_index3 + 1, FACTORS_NUMBER):
+                        for factor_index5 in range(factor_index4 + 1, FACTORS_NUMBER + 1):
+                            matrix[:, cur_factors_mult_index] = (matrix[:, factor_index1] * matrix[:, factor_index2] *
+                                                                 matrix[:, factor_index3] * matrix[:, factor_index4] *
+                                                                 matrix[:, factor_index5])
+                            cur_factors_mult_index += 1
+
+        assert cur_factors_mult_index == M_SIZE - 1
+        matrix[:, cur_factors_mult_index] = (matrix[:, 1] * matrix[:, 2] * matrix[:, 3] *
+                                             matrix[:, 4] * matrix[:, 5] * matrix[:, 6])
 
         return matrix
 
